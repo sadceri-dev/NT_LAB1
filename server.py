bd5039c35b9033c48cc6ec511f658b602362a5df
sadceri <70819311+sadceri-dev@users.noreply.github.com>
2024-03-04 00:06:20 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(bytes("MAC-address: 00-14-22-01-23-45", "utf8"))
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
